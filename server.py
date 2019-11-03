ea57558bf92bcb5a179bd6e61d8d246316c00fea
Mohammed alJaser <aljaserm7@gmail.com>
2019-11-02 23:13:59 -0400

    First Flask framework trial. still working on it.

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('./index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('./about.html')
+
+@app.route('/blog')
+def blog():
+    return 'Hello, blog!'
+
+
+@app.route('/blog/2020/dogs')
+def blog2020():
+    return 'Hello, dog 2020!'
