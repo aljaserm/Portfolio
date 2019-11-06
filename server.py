548584d4690414d56a8f910b4634366f358efe85
Mohammed alJaser <aljaserm7@gmail.com>
2019-11-05 20:05:04 -0500

    more work on the website

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
@@ -7,15 +7,17 @@ def my_home():
     return render_template('./index.html')
 
 
-@app.route('/about.html')
-def about():
-    return render_template('./about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/works.html')
-def works():
-    return render_template('./works.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'try again'
 
-@app.route('/contact.html')
-def contact():
-    return render_template('./contact.html')
